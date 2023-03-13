ace946ecd0f87471d98ece9957639e55cdec54a8
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-13 19:52:15 -0300

    agrego carpeta components

---
+++ components/Header.js
@@ -0,0 +1,29 @@
+"use client";
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Image from 'next/image';
+
+export default function Header () {
+    return (
+        <>
+            <Box sx={{ flexGrow: 1}}>
+                <AppBar position="static" color='inherit'>
+                <Toolbar sx={{mt: 1}}>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    <Link href = "/" underline="none" color = "inherit">
+                        <Image src="/logo.png" alt='Foto' width="210" height="70" ml="60"/>
+                    </Link>
+                    </Typography>
+                    <Typography variant='h5' sx={{mr: 2}}>
+                    Fábrica de conductores eléctricos
+                    </Typography>
+                </Toolbar>
+                </AppBar>
+            </Box>
+        </>
+    )
+};
