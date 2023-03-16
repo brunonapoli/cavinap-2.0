ef9bff7186624dca614fa77215b3fa1874edb75f
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-16 00:10:42 -0300

    agrego algunos datos a la tabla

--- app/unipolar/page.js
+++ app/unipolar/page.js
@@ -1,34 +1,45 @@
 "use client";
 
 import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { Container, Typography } from '@mui/material';
+import { Container, Typography, TableBody } from '@mui/material';
 
-const medidas = [
-    0.75, 1.00, 1.50, 2.50, 4, 6, 10
-]
+// import {medidas, titulos, diametro, tipo, espesor, diametroExt, resistencia, corriente, peso, fusible} from '../../utils/datos';
+import {datos, titulos} from '../../utils/datos';
 
 export default function  Unipolar() {
     return (
         <>
-        <Container maxWidth="md" sx={{mt:4}}>
+        <Container sx={{mt:4}}>
             <TableContainer>
                 <Table>
                     <TableHead sx={{backgroundColor: 'black'}}>
                         <TableRow>
-                            {medidas.map((numero, index) => (
-                                <TableCell sx={{textAlign:'center'}}>
-                                    <Typography variant='h4' sx={{color: 'white'}}>
-                                        {numero}
+                            {titulos.map((titulo, index) => (
+                                <TableCell align='center'>
+                                    <Typography variant='h7' sx={{color: 'white'}}>
+                                        {titulo}
                                     </Typography>
                                 </TableCell>
                             ))}
                         </TableRow>
                     </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            {datos.map((dato, index) => (
+                                dato.map((valor) => (
+                                <TableCell align='center'>
+                                    <Typography variant='h7' sx={{color: 'black'}}>
+                                        {valor}
+                                    </Typography>
+                                </TableCell>
+                                ))
+                            ))}
+                        </TableRow>
+                    </TableBody>
                 </Table>
             </TableContainer>
         </Container>
