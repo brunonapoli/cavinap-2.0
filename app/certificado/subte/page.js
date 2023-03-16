10f2c59fbbc19ab90c194ea3074fda943e876f9a
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-16 20:30:09 -0300

    agrego titulo a todas las páginas

--- app/certificado/subte/page.js
+++ app/certificado/subte/page.js
@@ -1,8 +1,13 @@
 "use client";
 
+import {Container, Typography} from '@mui/material';
+
 export default function  CertSubte() {
     return (
         <>
+        <Container sx={{mt:3}}>
+            <Typography variant='h3'sx={{mb:3, textAlign:'center', textDecoration: 'underline'}}> Unipolares </Typography>
+        </ Container>
         </>
     );
 };
--- app/subte/page.js
+++ app/subte/page.js
@@ -1,8 +1,13 @@
 "use client";
 
-export default function  Subte() {
+import {Container, Typography} from '@mui/material';
+
+export default function  subte() {
     return (
         <>
+        <Container sx={{mt:3}}>
+            <Typography variant='h3'sx={{mb:3, textAlign:'center', textDecoration: 'underline'}}> Unipolares </Typography>
+        </ Container>
         </>
     );
 };
