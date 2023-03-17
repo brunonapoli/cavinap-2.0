c4c79f3949b1c507a34ac87cc56c663625bd9f5f
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-16 23:58:59 -0300

    datos de contacto agregados en card

--- app/nosotros/contacto/page.js
+++ app/nosotros/contacto/page.js
@@ -1,13 +1,41 @@
 "use client";
 
-import {Container, Typography} from '@mui/material';
+import { Container, Typography, Grid, Stack, Card, CardContent } from '@mui/material';
+import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from '@mui/icons-material/Email';
 
-export default function  contacto() {
+export default function contacto() {
     return (
         <>
-        <Container sx={{mt:3}}>
-            <Typography variant='h3'sx={{mb:3, textAlign:'center', textDecoration: 'underline'}}> Contacto </Typography>
-        </ Container>
+            <Container sx={{ mt: 3 }}>
+                <Typography variant='h3' sx={{ mb: 3, textAlign: 'center', textDecoration: 'underline' }}> Contacto </Typography>
+                <Typography variant='h5' sx={{ mb: 3 }}>Para mayor informacion nos puede contactar de las siguientes maneras:</Typography>
+                <Grid container spacing={1}>
+                    <Grid item xs={6}>
+                        <Card>
+                            <CardContent  >
+                                <Stack direction='row' alignItems='center' gap={1}>
+                                    <PhoneIcon sx={{ fontSize: 30 }} />
+                                    <Typography variant='h5'> Teléfono:</Typography>
+                                    <Typography variant='h5' sx={{ fontWeight: 'bold' }}> 4753 - 2919</Typography>
+                                </Stack>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Card>
+                            <CardContent>
+                                <Stack direction='row' alignItems='center' gap={1}>
+                                    <EmailIcon sx={{ fontSize: 30 }} />
+                                    <Typography variant='h5'> E-Mail: </Typography>
+                                    <Typography variant='h5' sx={{ fontWeight: 'bold' }}>info@cavinap.com.ar</Typography>
+                                </Stack>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </Grid>
+                <Typography variant='h5' sx={{ mb: 3, mt: 4, fontWeight: 'bold' }}>Lunes a Viernes de 09:00 hs. a 16:00 hs.</Typography>
+            </ Container>
         </>
     );
 };
