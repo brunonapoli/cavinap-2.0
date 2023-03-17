791feac7a260c7da6e0cd90796c2ad47b4d19c48
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-16 21:39:48 -0300

    agrego pagina de historia

---
+++ app/nosotros/historia/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import {Container, Typography} from '@mui/material';
+
+export default function  historia() {
+    return (
+        <>
+        <Container sx={{mt:3}}>
+            <Typography variant='h3'sx={{mb:3, textAlign:'center', textDecoration: 'underline'}}> Historia </Typography>
+        </ Container>
+        </>
+    );
+};
