4beb993bef8f150834323c78665b1d39ba15a124
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-16 21:36:39 -0300

    cambio titulo de las paginas

---
+++ app/nosotros/calidad/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import {Container, Typography} from '@mui/material';
+
+export default function  calidad() {
+    return (
+        <>
+        <Container sx={{mt:3}}>
+            <Typography variant='h3'sx={{mb:3, textAlign:'center', textDecoration: 'underline'}}> Calidad </Typography>
+        </ Container>
+        </>
+    );
+};
