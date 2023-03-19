7295b4d5d38cc8b3fade78b6d4c69890f4defc21
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-19 03:05:14 -0300

    datos unipolares

---
+++ utils/datosSub.js
@@ -0,0 +1,39 @@
+const titulos = [
+    "Sección Nominal (mm²)",
+    "Diámetro máximo alambres (mm)",
+    "Espesor Nominal aislación (mm)",
+    "Espesor Nominal vaina (mm)",
+    "Corriente máxima admisible aire o bandeja a 40°C(Amper)",
+    "Corriente máxima admisible enterrado(Amper)",
+    "Resistencia eléctrica máxima a 20°C (Ω/Km.)(Amper)",
+    "Diámetro exterior aproximado (mm)",
+    "Peso aproximado (Kg. x Km.)" 
+];
+
+const diametroMaximo = 0.41;
+const espesorVaina = 1.80;
+const espesorNominalChico = 0.80;
+const espesorNominalGrande = 1.00;
+
+const seccionBipolar = [
+    "2x1,5", "2x2,5", "2x4", "2x6", "2x10"
+];
+
+const seccionTripolar = [
+    "3x1,5", "3x2,5", "3x4", "3x6", "3x10"
+];
+
+const seccionTetrapolar = [
+    "41,5", "4x2,5", "4x4", "4x10"
+];
+
+const corrienteAire = [15, 21, 28, 37, 50];
+const corrienteEnterrado = [25, 35, 44, 56, 72];
+
+const resistencia = [13.30, 7.98, 4.95, 3.30, 1.91];
+
+const diametroExtBipolar = [9.50, 10.60, 12.30, 12.90, 16.20];
+const diametroExtTripolar = [10.20, 11.80, 12.80, 13.90, 17.50];
+const diametroExtTetrapolar = [10.90, 12.40, 13.90, 15.80, 19];
+
+
