4bf0daa9b32e8d17adaef8cf36c1d4c6be6c81e4
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-19 15:47:40 -0300

    tabla con datos de los subtes

--- app/subte/page.js
+++ app/subte/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { titulos } from '@/utils/datosSub';
+import { titulos, datosBipolar, datosTripolar, datosTetrapolar } from '@/utils/datosSub';
 
 import Table from '@mui/material/Table';
 import TableCell from '@mui/material/TableCell';
@@ -23,58 +23,75 @@ export default function subte() {
                     EXTRUIDOS PARA TENSIONES NOMINALES HASTA 1,1 KV CERTIFICADOS SEGÚN NORMA IRAM 2178/04
                 </Typography>
                 <TableContainer>
-                    <Table>
+                    <Table sx={{ mb: 3 }}>
+
                         <TableHead sx={{ backgroundColor: 'black', border: '1px solid grey' }}>
                             <TableRow>
-                            {titulos.map((titulo) => (
-                                <TableCell align='center'>
-                                    <Typography variant='h8' color={'white'}> {titulo} </Typography>
-                                </TableCell>
+                                {titulos.map((titulo) => (
+                                    <TableCell align='center'>
+                                        <Typography variant='h8' color={'white'}> {titulo} </Typography>
+                                    </TableCell>
                                 ))}
                             </TableRow>
                         </TableHead>
+
                         <TableHead sx={{ backgroundColor: '#000010', border: '1px solid grey' }}>
                             <TableRow>
                                 <TableCell align='center' colSpan={titulos.length}>
-                                    <Typography variant='h5' color={'white'}> Bipolar </Typography>
+                                    <Typography variant='h7' color={'white'}> Bipolar </Typography>
                                 </TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            <TableRow>
-                                <TableCell>
-
-                                </TableCell>
-                            </TableRow>
+                            {datosBipolar.map((datos) => (
+                                <TableRow>
+                                    {datos.map((datoBipolar) => (
+                                        <TableCell align='center'>
+                                            {datoBipolar}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))}
                         </TableBody>
+
                         <TableHead sx={{ backgroundColor: '#000010', border: '1px solid grey' }}>
                             <TableRow>
                                 <TableCell align='center' colSpan={titulos.length}>
-                                    <Typography variant='h5' color={'white'}> Tripolar </Typography>
+                                    <Typography variant='h7' color={'white'}> Tripolar </Typography>
                                 </TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            <TableRow>
-                                <TableCell>
-
-                                </TableCell>
-                            </TableRow>
+                            {datosTripolar.map((datos) => (
+                                <TableRow>
+                                    {datos.map((datoTripolar) => (
+                                        <TableCell align='center'>
+                                            {datoTripolar}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))}
                         </TableBody>
+
                         <TableHead sx={{ backgroundColor: '#000010', border: '1px solid grey' }}>
                             <TableRow>
                                 <TableCell align='center' colSpan={titulos.length}>
-                                    <Typography variant='h5' color={'white'}> Tetrapolar </Typography>
+                                    <Typography variant='h7' color={'white'}> Tetrapolar </Typography>
                                 </TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            <TableRow>
-                                <TableCell>
-
-                                </TableCell>
-                            </TableRow>
+                            {datosTetrapolar.map((datos) => (
+                                <TableRow>
+                                    {datos.map((datoTetrapolar) => (
+                                        <TableCell align='center'>
+                                            {datoTetrapolar}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))}
                         </TableBody>
+
                     </Table>
                 </TableContainer>
             </ Container>
