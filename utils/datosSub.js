d9ad0b2d0f6cc99f266a81d5e449e7346c158c02
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-19 15:47:27 -0300

    array con datos de los subtes

--- utils/datosSub.js
+++ utils/datosSub.js
@@ -24,7 +24,7 @@ const seccionTripolar = [
 ];
 
 const seccionTetrapolar = [
-    "41,5", "4x2,5", "4x4", "4x10"
+    "4x1,5", "4x2,5", "4x4", "4x6", "4x10"
 ];
 
 const corrienteAire = [15, 21, 28, 37, 50];
@@ -36,4 +36,24 @@ const diametroExtBipolar = [9.50, 10.60, 12.30, 12.90, 16.20];
 const diametroExtTripolar = [10.20, 11.80, 12.80, 13.90, 17.50];
 const diametroExtTetrapolar = [10.90, 12.40, 13.90, 15.80, 19];
 
+const pesoBipolar = [118, 175, 234, 290, 430];
+const pesoTripolar = [155, 188, 278, 360, 550];
+const pesoTetrapolar = [178, 244, 370, 450, 670];
 
+const datosBipolar = [];
+const datosTripolar = [];
+const datosTetrapolar = [];
+
+for (let i = 0; i < 5; i++) {
+    if (seccionBipolar[i].toString().includes("4") || seccionBipolar[i].toString().includes("6") || seccionBipolar[i].toString().includes("10")) {
+        datosBipolar.push([seccionBipolar[i], diametroMaximo, espesorNominalGrande, espesorVaina, corrienteAire[i], corrienteEnterrado[i], resistencia[i], diametroExtBipolar[i], pesoBipolar[i]])
+        datosTripolar.push([seccionTripolar[i], diametroMaximo, espesorNominalGrande, espesorVaina, corrienteAire[i], corrienteEnterrado[i], resistencia[i], diametroExtTripolar[i], pesoTripolar[i]])
+        datosTetrapolar.push([seccionTetrapolar[i], diametroMaximo, espesorNominalGrande, espesorVaina, corrienteAire[i], corrienteEnterrado[i], resistencia[i], diametroExtTetrapolar[i], pesoTetrapolar[i]])
+    } else {
+        datosBipolar.push([seccionBipolar[i], diametroMaximo, espesorNominalChico, espesorVaina, corrienteAire[i], corrienteEnterrado[i], resistencia[i], diametroExtBipolar[i], pesoBipolar[i]])
+        datosTripolar.push([seccionTripolar[i], diametroMaximo, espesorNominalChico, espesorVaina, corrienteAire[i], corrienteEnterrado[i], resistencia[i], diametroExtTripolar[i], pesoTripolar[i]])
+        datosTetrapolar.push([seccionTetrapolar[i], diametroMaximo, espesorNominalChico, espesorVaina, corrienteAire[i], corrienteEnterrado[i], resistencia[i], diametroExtTetrapolar[i], pesoTetrapolar[i]])
+    }
+}
+
+export {titulos, datosBipolar, datosTripolar, datosTetrapolar}
