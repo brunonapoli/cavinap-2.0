aed3c2bf2f1684ecd106d824f616a9eed5e6b032
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-21 10:34:39 -0300

    nuevo footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -2,19 +2,22 @@
 
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
-import { BottomNavigationAction } from '@mui/material';
+import { BottomNavigationAction, Toolbar, Stack, AppBar } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
 export default function Footer() {
     return (
         <>
             <Box sx={{ width: '100%', bottom: '0' }}>
-                <BottomNavigation sx={{ borderTop: '0.5px solid grey' }}>
-                    <BottomNavigationAction label= 'Copyright © 2023 Cavinap - All Rights Reserved'/>
-                    {/* <Typography variant='h7' sx={{ mt: '18px' }}>
-                        Copyright © 2023 Cavinap - All Rights Reserved
-                    </Typography> */}
-                </BottomNavigation>
+                <AppBar position='static' color='inherit' sx={{alignItems: 'center' }}>
+                    <Toolbar>
+                        <Stack direction='row' alignItems='center'>
+                            <Typography variant='h7' sx={{ textAlign: 'center' }}>
+                                Copyright © 2023 Cavinap - All Rights Reserved
+                            </Typography>
+                        </Stack>
+                    </Toolbar>
+                </AppBar>
             </Box>
         </>
     );
