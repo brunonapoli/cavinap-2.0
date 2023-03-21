81c5847bbb389345aa5e6647b2c586e28029d501
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-20 21:37:39 -0300

    pagina principal con cards y centradas

--- app/page.js
+++ app/page.js
@@ -5,52 +5,92 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import { Card, CardContent, Stack } from '@mui/material';
 import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function main() {
   return (
     <>
-      <Box sx={{ flexGrow: 1, m:5}}>
-        <Grid container spacing={3}>
-          <Grid item xs={6}>
-            <Link href = "/unipolar" sx={{border: '1px solid #ef5350'}}>
-              <Typography variant='h2'sx={{textAlign: 'center'}}>
-                Unipolares 
-              </Typography>
-              <Typography variant='h5' sx={{textAlign: 'center'}}>Datos técnicos unipolares</Typography>
+      <Box sx={{ flexGrow: 1, m: 4 }}>
+        <Grid container spacing={2} justifyContent='center'>
+          <Grid item xs={4}>
+            <Link href="/unipolar">
+              <Card>
+                <CardContent>
+                  <Typography variant='h4' sx={{ textAlign: 'center' }}> Unipolares </Typography>
+                  <Typography variant='h6' sx={{ textAlign: 'center' }}>Datos técnicos unipolares</Typography>
+                </CardContent>
+              </Card>
             </Link>
           </Grid>
-          <Grid item xs={6}>
-            <Link href = "/subte">
-              <Typography variant='h2'sx={{textAlign: 'center', border: '1px solid #ef5350'}}> Subterráneos </Typography>
+
+          <Grid item xs={4}>
+            <Link href="/subte">
+              <Card>
+                <CardContent>
+                  <Typography variant='h4' sx={{ textAlign: 'center' }}> Subterráneos </Typography>
+                  <Typography variant='h6' sx={{ textAlign: 'center' }}>Datos técnicos subterráneos</Typography>
+                </CardContent>
+              </Card>
             </Link>
           </Grid>
-          <Grid item xs={6}>
-            <Link href = "/certificado/unipolar">
-              <Typography variant='h2'sx={{textAlign: 'center', border: '1px solid #ef5350'}}> Cert. Unipolares </Typography>
+        </ Grid>
+
+        <Grid container spacing={2} justifyContent='center' sx={{mt:1}}>
+          <Grid item xs={4}>
+            <Link href="/certificado/unipolar">
+              <Card>
+                <CardContent>
+                  <Typography variant='h4' sx={{ textAlign: 'center' }}> Cert. Unipolares </Typography>
+                  <Typography variant='h6' sx={{ textAlign: 'center' }}>Certificado unipolares</Typography>
+                </CardContent>
+              </Card>
             </Link>
           </Grid>
-          <Grid item xs={6}>
-            <Link href = "/certificado/subte">
-              <Typography variant='h2'sx={{textAlign: 'center', border: '1px solid #ef5350'}}> Cert. Subterráneos </Typography>
+          <Grid item xs={4}>
+            <Link href="/certificado/subte">
+              <Card>
+                <CardContent>
+                  <Typography variant='h4' sx={{ textAlign: 'center' }}> Cert. Subterráneos </Typography>
+                  <Typography variant='h6' sx={{ textAlign: 'center' }}>Certificado subterráneos</Typography>
+                </CardContent>
+              </Card>
             </Link>
           </Grid>
-          <Grid item xs={12}>
-            <Link href = "/nosotros/calidad">
-              <Typography variant='h2'sx={{textAlign: 'center', border: '1px solid #ef5350'}}> Calidad </Typography>
+        </Grid>
+        <Grid container spacing={2} justifyContent='center' sx={{mt:1}}>
+          <Grid item xs={8}>
+            <Link href="/nosotros/calidad">
+              <Card>
+                <CardContent>
+                  <Typography variant='h4' sx={{ textAlign: 'center' }}> Calidad </Typography>
+                  <Typography variant='h6' sx={{ textAlign: 'center' }}>Significado de calidad en Cavinap</Typography>
+                </CardContent>
+              </Card>
             </Link>
           </Grid>
-          <Grid item xs={6}>
-            <Link href= "/nosotros/contacto">
-              <Typography variant='h2'sx={{textAlign: 'center', border: '1px solid #ef5350'}}> Contáctenos </Typography>
+        </Grid>
+        <Grid container spacing={2} justifyContent='center' sx={{mt:1}}>
+          <Grid item xs={4}>
+            <Link href="/nosotros/contacto">
+              <Card>
+                <CardContent>
+                  <Typography variant='h4' sx={{ textAlign: 'center' }}> Contáctenos </Typography>
+                  <Typography variant='h6' sx={{ textAlign: 'center' }}>Datos para contactarse con nosotros</Typography>
+                </CardContent>
+              </Card>
             </Link>
           </Grid>
-          <Grid item xs={6}>
-            <Link href = "/nosotros/historia">
-              <Typography variant='h2'sx={{textAlign: 'center', border: '1px solid #ef5350'}}> Nuestra historia </Typography>
+          <Grid item xs={4}>
+            <Link href="/nosotros/historia">
+              <Card>
+                <CardContent>
+                  <Typography variant='h4' sx={{ textAlign: 'center' }}> Nuestra historia </Typography>
+                  <Typography variant='h6' sx={{ textAlign: 'center' }}>Pequeña historia sobre nuestros inicios</Typography>
+                </CardContent>
+              </Card>
             </Link>
           </Grid>
         </Grid>
