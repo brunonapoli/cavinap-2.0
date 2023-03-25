240d92ac201f50c88fa104e2c6a22ef6608321e4
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-25 01:25:29 -0300

    agrego imagen a las cards

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
-import { Card, CardContent, CardMedia } from '@mui/material';
+import { Card, CardContent, CardMedia, Avatar, Stack } from '@mui/material';
 import Link from 'next/link';
 
 export default function main() {
@@ -15,10 +15,9 @@ export default function main() {
           <Grid item xs={9} md={4}>
             <Link href="/unipolar">
               <Card>
-                <CardMedia component={'img'} height='90' image='/head1.gif' alt='Imagen'/>
+                <CardMedia component={'img'} height='90' image='/head1.gif' alt='Imagen' />
                 <CardContent>
                   <Typography variant='h4' sx={{ textAlign: 'center' }}> Unipolares </Typography>
-                  {/* <Typography variant='h6' sx={{ textAlign: 'center' }}>Datos técnicos unipolares</Typography> */}
                 </CardContent>
               </Card>
             </Link>
@@ -27,23 +26,24 @@ export default function main() {
           <Grid item xs={9} md={4}>
             <Link href="/subte">
               <Card>
-              <CardMedia component={'img'} height='90' image='/head4.gif' alt='Imagen'/>
+                <CardMedia component={'img'} height='90' image='/head4.gif' alt='Imagen' />
                 <CardContent>
                   <Typography variant='h4' sx={{ textAlign: 'center' }}> Subterráneos </Typography>
-                  {/* <Typography variant='h6' sx={{ textAlign: 'center' }}>Datos técnicos subterráneos</Typography> */}
                 </CardContent>
               </Card>
             </Link>
           </Grid>
         </ Grid>
 
-        <Grid container spacing={2} justifyContent='center' sx={{mt:1}}>
+        <Grid container spacing={2} justifyContent='center' sx={{ mt: 1 }}>
           <Grid item xs={9} md={4}>
             <Link href="/certificado/unipolar">
               <Card>
                 <CardContent>
-                  <Typography variant='h4' sx={{ textAlign: 'center' }}> Cert. Unipolares </Typography>
-                  <Typography variant='h6' sx={{ textAlign: 'center' }}>Certificado unipolares</Typography>
+                  <Stack direction='row' alignItems='center' justifyContent='center' gap={1}>
+                    <Avatar alt="Certificado" src='/iram.jpg' sx={{ width: 60, height: 60 }} />
+                    <Typography variant='h4' sx={{ textAlign: 'center' }}> Cert. Unipolares </Typography>
+                  </Stack>
                 </CardContent>
               </Card>
             </Link>
@@ -52,33 +52,34 @@ export default function main() {
             <Link href="/certificado/subte">
               <Card>
                 <CardContent>
-                  <Typography variant='h4' sx={{ textAlign: 'center' }}> Cert. Subterráneos </Typography>
-                  <Typography variant='h6' sx={{ textAlign: 'center' }}>Certificado subterráneos</Typography>
+                  <Stack direction='row' alignItems='center' justifyContent='center' gap={1}>
+                    <Avatar alt="Certificado" src='/iram.jpg' sx={{ width: 60, height: 60 }} />
+                    <Typography variant='h4' sx={{ textAlign: 'center' }}> Cert. Subterráneos </Typography>
+                  </Stack>
                 </CardContent>
               </Card>
             </Link>
           </Grid>
         </Grid>
-        <Grid container spacing={2} justifyContent='center' sx={{mt:1}}>
+        <Grid container spacing={2} justifyContent='center' sx={{ mt: 1 }}>
           <Grid item xs={9} md={8}>
             <Link href="/nosotros/calidad">
               <Card>
-              <CardMedia component={'img'} height='60' image='/head5.gif' alt='Imagen'/>
                 <CardContent>
                   <Typography variant='h4' sx={{ textAlign: 'center' }}> Calidad </Typography>
-                  {/* <Typography variant='h6' sx={{ textAlign: 'center' }}>Significado de calidad en Cavinap</Typography> */}
+                  <Typography variant='h6' sx={{ textAlign: 'center' }}>Significado de calidad en Cavinap</Typography>
                 </CardContent>
               </Card>
             </Link>
           </Grid>
         </Grid>
-        <Grid container spacing={2} justifyContent='center' sx={{mt:1}}>
+        <Grid container spacing={2} justifyContent='center' sx={{ mt: 1 }}>
           <Grid item xs={9} md={4}>
             <Link href="/nosotros/contacto">
               <Card>
+                <CardMedia component={'img'} height='65' image='/contacto.jpg' alt='Imagen' />
                 <CardContent>
                   <Typography variant='h4' sx={{ textAlign: 'center' }}> Contáctenos </Typography>
-                  <Typography variant='h6' sx={{ textAlign: 'center' }}>Datos para contactarse con nosotros</Typography>
                 </CardContent>
               </Card>
             </Link>
@@ -86,16 +87,16 @@ export default function main() {
           <Grid item xs={9} md={4}>
             <Link href="/nosotros/historia">
               <Card>
+                <CardMedia component={'img'} height='65' image='/head5.gif' alt='Imagen' />
                 <CardContent>
                   <Typography variant='h4' sx={{ textAlign: 'center' }}> Nuestra historia </Typography>
-                  <Typography variant='h6' sx={{ textAlign: 'center' }}>Pequeña historia sobre nuestros inicios</Typography>
                 </CardContent>
               </Card>
             </Link>
           </Grid>
         </Grid>
       </Box>
-      <Footer posicion={'static'}/>
+      <Footer posicion={'static'} />
     </>
   )
 }
