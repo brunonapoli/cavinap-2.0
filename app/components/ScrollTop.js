1642a747a6a50045977ae3a213509ec70d7c9e81
Bruno Napoli <napolii.bruno@gmail.com>
2023-03-26 23:22:34 -0300

    cambio color

--- app/components/ScrollTop.js
+++ app/components/ScrollTop.js
@@ -1,55 +1,63 @@
 'use client';
 
-import MenuIcon from '@mui/icons-material/Menu';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Slide from '@mui/material/Slide';
 import Fade from '@mui/material/Fade';
 import Box from '@mui/material/Box';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-import { Button, IconButton } from '@mui/material';
+import { IconButton } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+      primary: {
+          main: '#ef5350',
+      },
+  },
+});
 
 function ScrollTop(props) {
-    const { children, window } = props;
-    const trigger = useScrollTrigger({
-      target: window ? window() : undefined,
-      disableHysteresis: true,
-      threshold: 100,
-    });
-  
-    const handleClick = (event) => {
-      const anchor = (event.target.ownerDocument || document).querySelector(
-        '#back-to-top-anchor',
-      );
-  
-      if (anchor) {
-        anchor.scrollIntoView({
-          block: 'center',
-        });
-      }
-    };
-  
-    return (
-      <Fade in={trigger}>
-        <Box
-          onClick={handleClick}
-          role="presentation"
-          sx={{ position: 'fixed', bottom: 80, right: 100 }}
-        >
-          {children}
-        </Box>
-      </Fade>
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      '#back-to-top-anchor',
     );
-  }
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'center',
+      });
+    }
+  };
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 80, right: 100 }}
+      >
+        {children}
+      </Box>
+    </Fade>
+  );
+}
 
 export default function ScrollToTop(props) {
-    return (
-        <>
-            <ScrollTop {...props}>
-                {/* <Button variant="contained">Contained</Button> */}
-                <IconButton color='primary' sx={{background: 'black', height:50, width:50}}>
-                    <KeyboardArrowUpIcon sx={{height:50, width:50}}/>
-                </IconButton>
-            </ScrollTop>
-        </>
-    )
+  return (
+    <>
+      <ScrollTop {...props}>
+        <ThemeProvider theme={theme}>
+          <IconButton sx={{ backgroundColor:'#ef5350',height: 50, width: 50 }}>
+            <KeyboardArrowUpIcon sx={{color:'#fff', height: 50, width: 50 }} />
+          </IconButton>
+        </ThemeProvider>
+      </ScrollTop>
+    </>
+  )
 };
