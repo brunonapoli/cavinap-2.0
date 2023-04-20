1a78dea2c3f2e9e1c18190af0ac9083d2fa953d0
Bruno Napoli <napolii.bruno@gmail.com>
2023-04-20 20:10:40 -0300

    cambio de color de fondo y marcado

--- app/components/Bar.js
+++ app/components/Bar.js
@@ -5,6 +5,19 @@ import Link from 'next/link';
 import AppBar from '@mui/material/AppBar';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            // main: '#E80C0C',
+            main: 'rgba(183, 18, 52)'
+        },
+        secondary: {
+            main: '#FFBE15'
+        }
+    },
+});
 
 function LinkTab(props) {
     return (
@@ -27,24 +40,27 @@ export default function Bar() {
 
     return (
         <>
-            <AppBar position="static">
-                <Tabs
-                    value={value}
-                    onChange={handleChange}
-                    indicatorColor="secondary"
-                    textColor="inherit"
-                    variant="fullWidth"
-                    aria-label="full width tabs example"
-                >
-                    <LinkTab label="Unipolares" href="/unipolar"/>
-                    <LinkTab label="Subterráneos" href="/subte"/>
-                    <LinkTab label="Certificado Unipolares" href="/certificado/unipolar"/>
-                    <LinkTab label="Certificado Subterráneos" href="/certificado/subte"/>
-                    <LinkTab label="Calidad" href="/nosotros/calidad"/>
-                    <LinkTab label="Contacto" href="/nosotros/contacto"/>
-                    <LinkTab label="Nuestra historia" href="/nosotros/historia"/>
-                </Tabs>
-            </AppBar>
+            <ThemeProvider theme={theme}>
+                <AppBar position="static">
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        indicatorColor="secondary"
+                        // textColor="secondary"
+                        textColor="inherit"
+                        variant="fullWidth"
+                        aria-label="full width tabs example"
+                    >
+                        <LinkTab label="Unipolares" href="/unipolar" />
+                        <LinkTab label="Subterráneos" href="/subte" />
+                        <LinkTab label="Certificado Unipolares" href="/certificado/unipolar" />
+                        <LinkTab label="Certificado Subterráneos" href="/certificado/subte" />
+                        <LinkTab label="Calidad" href="/nosotros/calidad" />
+                        <LinkTab label="Contacto" href="/nosotros/contacto" />
+                        <LinkTab label="Nuestra historia" href="/nosotros/historia" />
+                    </Tabs>
+                </AppBar>
+            </ThemeProvider>
         </>
     )
 };
