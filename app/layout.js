a25261398295acae81052ba30684733125adf2ee
Bruno Napoli <napolii.bruno@gmail.com>
2023-05-14 22:08:52 -0300

    codigo limpio

--- app/layout.js
+++ app/layout.js
@@ -5,14 +5,7 @@ import ScrollToTop from './components/ScrollTop';
 export const metadata = {
   title: 'Cavinap',
   description: "Cavinap's page",
-  // icons: {
-  //   icon: '/wiring.ico',
-  // },
 }
-
-//EL METADATA ME SOBREESCRIBE EL COMPONENTE HEAD, NEXTJS 13 TRABAJA CON ESTA VARIABLE PARA CAMBIAR
-//TANTO EL TÍTULO, COMO SU FOTO Y CONTENIDO. PARA LAS IMAGENES LA FOTO TIENE QUE TENER LOS SIGUIENTES TITULOS:
-//favicon.ico, icon.(ico|jpg|jpeg|png|svg) or apple-icon.(jpg|jpeg|png|svg)
 
 export default function RootLayout({ children }) {
   return (
