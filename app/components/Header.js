506d4c9d1e545b72594bf9fcc395afeecb891ce8
Bruno Napoli <napolii.bruno@gmail.com>
2023-09-25 23:46:15 -0300

    header responsive

--- app/components/Header.js
+++ app/components/Header.js
@@ -10,15 +10,15 @@ import Image from 'next/image';
 export default function Header() {
     return (
         <>
-            <Box sx={{ flexGrow: 1 }} id="back-to-top-anchor">
+            <Box sx={{ flexGrow: 1 }} id="back-to-top-anchor" >
                 <AppBar position="static" color='inherit'>
-                    <Toolbar sx={{ mt: 1 }}>
-                        <Typography variant="h6" component="div" sx={{ flexGrow: 1, ml: 2 }}>
+                    <Toolbar sx={{ mt: 1, pl: { xs: 0 } }} >
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1, pl: { xs: 1, sm: 2 } }}>
                             <Link href="/">
                                 <Image src="/logo.png" alt='Foto' width="210" height="70" ml="60" />
                             </Link>
                         </Typography>
-                        <Typography variant='h5' sx={{ mr: 2, ml:5 }}>
+                        <Typography variant='h5' sx={{ mr: 2, pl: { xs: 1, sm: 2, sm: 3 } }}>
                             Fábrica de conductores eléctricos
                         </Typography>
                     </Toolbar>
