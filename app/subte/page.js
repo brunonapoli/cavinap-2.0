c0839da604fe8fc950dc5e1a8fbaed3ac44454ae
Bruno Napoli <napolii.bruno@gmail.com>
2023-09-27 19:48:45 -0300

    cambio fontSize y resalto medidas

--- app/subte/page.js
+++ app/subte/page.js
@@ -14,7 +14,7 @@ import { Container, Typography, TableBody, Box } from '@mui/material';
 export default function subte() {
     return (
         <>
-            <Bar posicion={2} posicionTabIndicator={64 * 3}/>
+            <Bar posicion={2} posicionTabIndicator={64 * 3} />
             <Container sx={{ mt: 3 }}>
                 <Typography variant='h3' sx={{ mb: 3, textAlign: 'center', textDecoration: 'underline' }}> Subterráneos </Typography>
                 <Typography variant='h7' paragraph>
@@ -41,7 +41,7 @@ export default function subte() {
                         <TableHead sx={{ backgroundColor: '#000010', border: '1px solid grey' }}>
                             <TableRow>
                                 <TableCell align='center' colSpan={titulos.length}>
-                                    <Typography variant='h7' color={'white'}>
+                                    <Typography variant='h7' color={'white'} sx={{fontSize: '2rem'}}>
                                         Bipolar
                                     </Typography>
                                 </TableCell>
@@ -51,9 +51,14 @@ export default function subte() {
                             {datosBipolar.map((datos) => (
                                 <TableRow>
                                     {datos.map((datoBipolar, index) => (
-                                        <TableCell align='center' sx={{ border: '1px solid grey' }} key={index}>
-                                            {datoBipolar}
-                                        </TableCell>
+                                        (datoBipolar == "2x1,5") || (datoBipolar == "2x2,5") || (datoBipolar == "2x4") || (datoBipolar =="2x6") || (datoBipolar =="2x10") ?
+                                            <TableCell align='center' sx={{ fontSize: '1.4rem', border: '1px solid grey', fontWeight: 'bold' }} key={index}>
+                                                {datoBipolar}
+                                            </TableCell>
+                                            :
+                                            <TableCell align='center' sx={{ fontSize: '1.2rem', border: '1px solid grey' }} key={index}>
+                                                {datoBipolar}
+                                            </TableCell>
                                     ))}
                                 </TableRow>
                             ))}
@@ -62,7 +67,7 @@ export default function subte() {
                         <TableHead sx={{ backgroundColor: '#000010', border: '1px solid grey' }}>
                             <TableRow>
                                 <TableCell align='center' colSpan={titulos.length}>
-                                    <Typography variant='h7' color={'white'}>
+                                    <Typography variant='h7' color={'white'} sx={{fontSize: '2rem'}}>
                                         Tripolar
                                     </Typography>
                                 </TableCell>
@@ -71,19 +76,24 @@ export default function subte() {
                         <TableBody sx={{ backgroundColor: 'white' }}>
                             {datosTripolar.map((datos) => (
                                 <TableRow>
-                                    {datos.map((datoTripolar, index) => (
-                                        <TableCell align='center' sx={{ border: '1px solid grey' }} key={index}>
+                                {datos.map((datoTripolar, index) => (
+                                    (datoTripolar == "3x1,5") || (datoTripolar == "3x2,5") || (datoTripolar == "3x4") || (datoTripolar =="3x6") || (datoTripolar =="3x10") ?
+                                        <TableCell align='center' sx={{ fontSize: '1.4rem', border: '1px solid grey', fontWeight: 'bold' }} key={index}>
                                             {datoTripolar}
                                         </TableCell>
-                                    ))}
-                                </TableRow>
+                                        :
+                                        <TableCell align='center' sx={{ fontSize: '1.2rem', border: '1px solid grey' }} key={index}>
+                                            {datoTripolar}
+                                        </TableCell>
+                                ))}
+                            </TableRow>
                             ))}
                         </TableBody>
 
                         <TableHead sx={{ backgroundColor: '#000010', border: '1px solid grey' }}>
                             <TableRow>
                                 <TableCell align='center' colSpan={titulos.length}>
-                                    <Typography variant='h7' color={'white'}>
+                                    <Typography variant='h7' color={'white'} sx={{fontSize: '2rem'}}>
                                         Tetrapolar
                                     </Typography>
                                 </TableCell>
@@ -92,12 +102,17 @@ export default function subte() {
                         <TableBody sx={{ backgroundColor: 'white' }}>
                             {datosTetrapolar.map((datos) => (
                                 <TableRow>
-                                    {datos.map((datoTetrapolar, index) => (
-                                        <TableCell align='center' sx={{ border: '1px solid grey' }} key={index}>
+                                {datos.map((datoTetrapolar, index) => (
+                                    (datoTetrapolar == "4x1,5") || (datoTetrapolar == "4x2,5") || (datoTetrapolar == "4x4") || (datoTetrapolar =="4x6") || (datoTetrapolar =="4x10") ?
+                                        <TableCell align='center' sx={{ fontSize: '1.4rem', border: '1px solid grey', fontWeight: 'bold' }} key={index}>
                                             {datoTetrapolar}
                                         </TableCell>
-                                    ))}
-                                </TableRow>
+                                        :
+                                        <TableCell align='center' sx={{ fontSize: '1.2rem', border: '1px solid grey' }} key={index}>
+                                            {datoTetrapolar}
+                                        </TableCell>
+                                ))}
+                            </TableRow>
                             ))}
                         </TableBody>
                     </Table>
@@ -106,42 +121,42 @@ export default function subte() {
                 <Table sx={{ mb: 3 }}>
                     <TableHead sx={{ backgroundColor: 'black', border: '1px solid grey' }}>
                         <TableRow>
-                            <TableCell align='center' colSpan={3} sx={{ color: 'white' }}>
+                            <TableCell align='center' colSpan={3} sx={{ color: 'white', fontSize: '1.7rem' }}>
                                 Colores de conductores aislantes
                             </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody sx={{ backgroundColor: 'white' }}>
                         <TableRow>
-                            <TableCell align='center' sx={{ border: '1px solid grey' }}>
+                            <TableCell align='center' sx={{ border: '1px solid grey', fontSize: '1.2rem' }}>
                                 Bipolar
                             </TableCell>
-                            <TableCell align='center' sx={{ border: '1px solid grey' }}>
+                            <TableCell align='center' sx={{ border: '1px solid grey', fontSize: '1.2rem' }}>
                                 Fase + Neutro
                             </TableCell>
-                            <TableCell align='center' sx={{ border: '1px solid grey' }}>
+                            <TableCell align='center' sx={{ border: '1px solid grey', fontSize: '1.2rem' }}>
                                 Marrón - Celeste
                             </TableCell>
                         </TableRow>
                         <TableRow>
-                            <TableCell align='center' sx={{ border: '1px solid grey' }}>
+                            <TableCell align='center' sx={{ border: '1px solid grey', fontSize: '1.2rem' }}>
                                 Tripolar
                             </TableCell>
-                            <TableCell align='center' sx={{ border: '1px solid grey' }}>
+                            <TableCell align='center' sx={{ border: '1px solid grey', fontSize: '1.2rem' }}>
                                 Tres fases
                             </TableCell>
-                            <TableCell align='center' sx={{ border: '1px solid grey' }}>
+                            <TableCell align='center' sx={{ border: '1px solid grey', fontSize: '1.2rem' }}>
                                 Marrón - Negro - Rojo
                             </TableCell>
                         </TableRow>
                         <TableRow>
-                            <TableCell align='center' sx={{ border: '1px solid grey' }}>
+                            <TableCell align='center' sx={{ border: '1px solid grey', fontSize: '1.2rem' }}>
                                 tetrapolar
                             </TableCell>
-                            <TableCell align='center' sx={{ border: '1px solid grey' }}>
+                            <TableCell align='center' sx={{ border: '1px solid grey', fontSize: '1.2rem' }}>
                                 Tres fases + Neutro
                             </TableCell>
-                            <TableCell align='center' sx={{ border: '1px solid grey' }}>
+                            <TableCell align='center' sx={{ border: '1px solid grey', fontSize: '1.2rem' }}>
                                 Marrón - Negro - Rojo - Celeste
                             </TableCell>
                         </TableRow>
