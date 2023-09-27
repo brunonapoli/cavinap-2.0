07e11d86f875c642fc15abff76f5c3759cb5d8d8
Bruno Napoli <napolii.bruno@gmail.com>
2023-09-26 21:01:00 -0300

    imagen responsive

--- app/certificado/subte/page.js
+++ app/certificado/subte/page.js
@@ -1,10 +1,10 @@
 "use client";
 
-import Image from 'next/image';
 import Link from 'next/link';
 import { Container, Typography, Stack } from '@mui/material';
 import Footer from '@/app/components/Footer';
 import Bar from '@/app/components/Bar';
+import Box from '@mui/material/Box';
 
 export default function CertSubte() {
     return (
@@ -14,16 +14,22 @@ export default function CertSubte() {
                 <Typography variant='h3' sx={{ mb: 3, textAlign: 'center', textDecoration: 'underline' }}>
                     Certificado Subterráneos
                 </Typography>
-                <Stack alignItems='center' sx={{ mb: 3 }}>
+                <Stack alignItems='center' sx={{ mb: 1 }}>
                     <Link href='/_next/image?url=%2FcertificadoSubte.jpg&w=750&q=75' target={'blank'}>
-                        <Image
-                            src="/certificadoSubte.jpg"
-                            alt='Foto del certificado del Subterráneo'
-                            width={660}
-                            height={902}
+                        <Box
+                            component="img"
+                            sx={{
+                                height: { xs: 451, sm: 739, md: 902 },
+                                width: { xs: 330, sm: 508, md: 660 }
+                            }}
+                            alt="Foto del certificado del Subterráneo"
+                            src="/certificadoUnipolar.jpg"
                         />
                     </Link>
                 </Stack>
+                <Typography variant='h6' paragraph sx={{ textAlign: 'center', mb: 2 }}>
+                    *Click en la imagen para mejor resolución.
+                </Typography>
             </ Container>
             <Footer posicion={'static'} />
         </>
--- app/certificado/unipolar/page.js
+++ app/certificado/unipolar/page.js
@@ -1,29 +1,36 @@
 "use client";
 
-import Image from 'next/image';
 import Link from 'next/link';
 import { Container, Typography, Stack } from '@mui/material';
 import Footer from '@/app/components/Footer';
-import Bar from '@/app/components/Bar';
+// import Bar from '@/app/components/Bar';
+import Bar from '@/app/components/Test';
+import Box from '@mui/material/Box';
 
 export default function certificadoUnipolar() {
     return (
         <>
-            <Bar posicion={3} />
+            <Bar posicion={3} posicionTabIndicator={64 * 4} />
             <Container sx={{ mt: 3 }}>
                 <Typography variant='h3' sx={{ mb: 3, textAlign: 'center', textDecoration: 'underline' }}>
                     Certificado Unipolares
                 </Typography>
-                <Stack alignItems='center' sx={{ mb: 3 }}>
+                <Stack alignItems='center' sx={{ mb: 1 }}>
                     <Link href='/_next/image?url=%2FcertificadoUnipolar.jpg&w=750&q=75' target={'blank'}>
-                        <Image
+                        <Box
+                            component="img"
+                            sx={{
+                                height: { xs: 451, sm: 739, md: 902 },
+                                width: { xs: 340, sm: 508, md: 680 }
+                            }}
+                            alt="Foto del certificado del Unipolar"
                             src="/certificadoUnipolar.jpg"
-                            alt='Foto del certificado del Unipolar'
-                            width={680}
-                            height={902}
                         />
                     </Link>
                 </Stack>
+                <Typography variant='h6' paragraph sx={{ textAlign: 'center', mb: 2 }}>
+                    *Click en la imagen para mejor resolución.
+                </Typography>
             </ Container>
             <Footer posicion={'static'} />
         </>
